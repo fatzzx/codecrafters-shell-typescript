6056b16c650004fe4cd3304944ef952ca4841f10
Felipe Farias <felipefariasmac@MacBook-Pro-de-Felipe.local>
2025-12-20 21:53:44 -0300

    cd-homeRef

--- a.ts
+++ a.ts
@@ -1,19 +1,3 @@
-import { delimiter, join } from "node:path";
-import { access, constants } from "node:fs/promises";
+import { delimiter } from "node:path";
 
-async function verifyIfTheExecExistsOrHasPermissions(value: string) {
-  const allDirs = Bun.env.PATH?.split(delimiter);
-  if (!allDirs) return null;
-  for (const dir of allDirs) {
-    const fullPath = join(dir, value);
-    try {
-      await access(fullPath, constants.X_OK);
-      return fullPath;
-    } catch {
-    }
-  }
-
-  return null;
-}
-
-console.log(await verifyIfTheExecExistsOrHasPermissions("grep"));
+console.log(Bun.env.HOME);
--- app/commands/cd.ts
+++ app/commands/cd.ts
@@ -1,8 +1,14 @@
 import printf from "../util/printf";
 import { stat } from "node:fs/promises";
 
+function handleHomeRef(path: string) {
+  if (path[0] == "~") return Bun.env.HOME + path.slice(1);
+  return path;
+}
+
 export default async function cd(path: string) {
   try {
+    path = handleHomeRef(path);
     const stats = await stat(path);
     if (stats.isDirectory()) process.chdir(path);
     else printf(`cd: ${path}: No such file or directory\n`);
